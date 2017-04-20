11ecc2fb2751df9317edfae696dd58c84880485f
41514181 <41514181@087930-P33S103.macklabs.br>
2017-04-20 10:33:00 -0300

    Separei os arquivos de cabeçalho e rodapé, arrumei a página de login e a index, coloquei a entidade que guardará as informações dos filmes/séries, Movie, e coloquei o Parser do XML

---
+++ OnlyTrackerApp/src/java/br/mack/lp3/controller/implemen/BuscaController.java
@@ -0,0 +1,32 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package br.mack.lp3.controller.implemen;
+
+import br.mack.lp3.controller.AbstractController;
+import br.mack.lp3.parsers.XMLParser;
+import br.mack.lp3.persistence.entities.Movie;
+import org.w3c.dom.Element;
+
+/**
+ *
+ * @author 41514181
+ */
+public class BuscaController extends AbstractController {
+
+    @Override
+    public void execute() {
+        XMLParser xmlpr = new XMLParser();
+        String mode = "xml";
+        xmlpr.openURL("http://www.omdbapi.com/?t="+this.getRequest().getParameter("q")+"&r="+mode);
+        
+        Element raiz = xmlpr.getDoc().getDocumentElement();
+        Movie mv = xmlpr.parseMovie(raiz);
+        
+        this.getRequest().getSession().setAttribute("movie", mv);
+        this.setReturnPage("index.jsp");
+    }
+    
+}
