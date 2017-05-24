ee7bfabc8c952956ef24f333ee0ad753804e6445
enrydell <henriquedell1@gmail.com>
2017-05-24 18:41:00 -0300

    Tentando colocar a função de adicionar filme/série ao usuário

--- src/java/br/mack/lp3/controller/implemen/CollectionAddController.java
+++ src/java/br/mack/lp3/controller/implemen/CollectionAddController.java
@@ -6,8 +6,6 @@
 package br.mack.lp3.controller.implemen;
 
 import br.mack.lp3.controller.AbstractController;
-import br.mack.lp3.persistence.ColecaoDAO;
-import br.mack.lp3.persistence.MovieDAO;
 import br.mack.lp3.persistence.UserLP3DAO;
 import br.mack.lp3.persistence.entities.Movie;
 import br.mack.lp3.persistence.entities.UserLP3;
@@ -24,11 +22,6 @@ import javax.naming.NamingException;
 public class CollectionAddController extends AbstractController {
 
     UserLP3DAO userLP3DAO = lookupUserLP3DAOBean();
-
-    MovieDAO movieDAO = lookupMovieDAOBean();
-
-    ColecaoDAO colecaoDAO = lookupColecaoDAOBean();
-
     
     @Override
     public void execute() {
@@ -40,26 +33,6 @@ public class CollectionAddController extends AbstractController {
 //        UserLP3 userAlterado = colecaoDAO.create(movie, user);
         this.getRequest().getSession().setAttribute("user", user);
         this.setReturnPage(this.getRequest().getContextPath() + "/user/feed.jsp");
-    }
-
-    private ColecaoDAO lookupColecaoDAOBean() {
-        try {
-            Context c = new InitialContext();
-            return (ColecaoDAO) c.lookup("java:global/OnlyTrackerApp/ColecaoDAO!br.mack.lp3.persistence.ColecaoDAO");
-        } catch (NamingException ne) {
-            Logger.getLogger(getClass().getName()).log(Level.SEVERE, "exception caught", ne);
-            throw new RuntimeException(ne);
-        }
-    }
-
-    private MovieDAO lookupMovieDAOBean() {
-        try {
-            Context c = new InitialContext();
-            return (MovieDAO) c.lookup("java:global/OnlyTrackerApp/MovieDAO!br.mack.lp3.persistence.MovieDAO");
-        } catch (NamingException ne) {
-            Logger.getLogger(getClass().getName()).log(Level.SEVERE, "exception caught", ne);
-            throw new RuntimeException(ne);
-        }
     }
 
     private UserLP3DAO lookupUserLP3DAOBean() {
