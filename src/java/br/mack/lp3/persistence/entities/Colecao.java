2238a81aaa24c06fe120c19b679e27fa2feeed27
enrydell <henriquedell1@gmail.com>
2017-05-24 18:12:01 -0300

    Tentando colocar a função de adicionar filme/série ao usuário

---
+++ src/java/br/mack/lp3/persistence/entities/Colecao.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package br.mack.lp3.persistence.entities;
+
+import java.io.Serializable;
+
+/**
+ *
+ * @author enrydell
+ */
+public class Colecao implements Serializable {
+    
+}
